840e37cc0fd8636815effdfd3c8fb55789263987
JeongJu Kim <haje01@gmail.com>
2019-01-30 10:38:35 +0900

    conform PEP8 & Python3

--- buffer.py
+++ buffer.py
@@ -1,46 +1,54 @@
+"""버퍼 모듈."""
 import numpy as np
 import random
 from collections import deque
 
 
 class MemoryBuffer:
+    """메모리 버퍼."""
 
-	def __init__(self, size):
-		self.buffer = deque(maxlen=size)
-		self.maxSize = size
-		self.len = 0
+    def __init__(self, size):
+        """초기화."""
+        self.buffer = deque(maxlen=size)
+        self.maxSize = size
+        self.len = 0
 
-	def sample(self, count):
-		"""
-		samples a random batch from the replay memory buffer
-		:param count: batch size
-		:return: batch (numpy array)
-		"""
-		batch = []
-		count = min(count, self.len)
-		batch = random.sample(self.buffer, count)
+    def sample(self, count):
+        """샘플링.
 
-		s_arr = np.float32([arr[0] for arr in batch])
-		a_arr = np.float32([arr[1] for arr in batch])
-		r_arr = np.float32([arr[2] for arr in batch])
-		s1_arr = np.float32([arr[3] for arr in batch])
+        samples a random batch from the replay memory buffer
 
-		return s_arr, a_arr, r_arr, s1_arr
+        :param count: batch size
+        :return: batch (numpy array)
+        """
+        batch = []
+        count = min(count, self.len)
+        batch = random.sample(self.buffer, count)
 
-	def len(self):
-		return self.len
+        s_arr = np.float32([arr[0] for arr in batch])
+        a_arr = np.float32([arr[1] for arr in batch])
+        r_arr = np.float32([arr[2] for arr in batch])
+        s1_arr = np.float32([arr[3] for arr in batch])
 
-	def add(self, s, a, r, s1):
-		"""
-		adds a particular transaction in the memory buffer
-		:param s: current state
-		:param a: action taken
-		:param r: reward received
-		:param s1: next state
-		:return:
-		"""
-		transition = (s,a,r,s1)
-		self.len += 1
-		if self.len > self.maxSize:
-			self.len = self.maxSize
-		self.buffer.append(transition)
+        return s_arr, a_arr, r_arr, s1_arr
+
+    def len(self):
+        """길이."""
+        return self.len
+
+    def add(self, s, a, r, s1):
+        """관측 추가.
+
+        adds a particular transaction in the memory buffer
+
+        :param s: current state
+        :param a: action taken
+        :param r: reward received
+        :param s1: next state
+        :return:
+        """
+        transition = (s, a, r, s1)
+        self.len += 1
+        if self.len > self.maxSize:
+            self.len = self.maxSize
+        self.buffer.append(transition)
